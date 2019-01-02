10c32462b998f75482b8a0cfb09f542bcfce31ae
Zulfikar Hidayatullah <45659177+VixiaZ@users.noreply.github.com>
2019-01-03 01:11:31 +0700

    Add files via upload

---
+++ Menghitung Upah kerja per jam.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+	int main ()
+	{
+		int jam;
+		
+		cout <<"-----Program penghitung upah kerja perjam By. Zulfikar Hidayatullah-----"<<endl;
+		
+		cout <<"\n  Masukan Jam kerja yang ingin dihitung : ";
+		cin >> jam;
+		
+		//rumus untuk menghitung upah kerja perjam
+		
+		cout <<"\nUpah kerja perjam anda adalah @ 5000/jam"<<endl;
+		
+		cout <<"\nupah Total jam kerja anda adalah = "<<5000*jam;
+		
+		return 0;
+		
+	}
